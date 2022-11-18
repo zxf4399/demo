005b711b42fe4c9b871f9648cc4930fd973b3aae
zxf4399 <zxf4399@gmail.com>
2022-11-18 22:05:13 +0800

    feat(webpack): add webpack

---
+++ webpack/src/index.ts
@@ -0,0 +1,8 @@
+import { Foo } from "./a";
+
+const foo: Foo = {
+  a: "a",
+  b: 1,
+};
+
+console.log(foo);
---
+++ webpack/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
